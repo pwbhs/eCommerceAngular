711eb7ecd7b3282adcc25b8e4af20f9830cdcff3
pwbhs <pwbhshshs@gmail.com>
2024-10-09 22:16:18 -0400

    Initial commit

---
+++ src/app/config/my-app-config.ts
@@ -0,0 +1,10 @@
+export default {
+
+    oidc: {
+        clientId: '0oa6gv5d36Uiy5O1t5d7',
+        issuer: 'https://dev-85756127.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+
+}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiBaseUrl: 'myecommerceapp-azdvczaje4dme5hw.canadaeast-01.azurewebsites.net/api'
+};
