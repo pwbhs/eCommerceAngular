77d38f9a6858c970637269d5476c32ccca14b68f
pwbhs <pwbhshshs@gmail.com>
2024-10-26 19:32:47 -0400

    try something

--- src/app/config/my-app-config.ts
+++ src/app/config/my-app-config.ts
@@ -5,7 +5,7 @@ export default {
     oidc: {
         clientId: '0oa6gv5d36Uiy5O1t5d7',
         issuer: 'https://dev-85756127.okta.com/oauth2/default',
-        redirectUri: `${environment.oidc.redirectUri}`,
+        redirectUri: 'https://ambitious-ground-04bd38a0f.5.azurestaticapps.net/login/callback',
         scopes: ['openid', 'profile', 'email']
     }
 
